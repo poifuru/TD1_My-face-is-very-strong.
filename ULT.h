f8c9f8b708df082920acc167fcdc3e50707330bd
大嶋大雅 <146915027+reachaso@users.noreply.github.com>
2024-11-21 14:25:09 +0900

    Create ULT.h

---
+++ ULT.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <math.h>
+#include <Vector2.h>
+#include <Novice.h>
+
+float easeInExpo(float x);
+
+void lerp(Vector2 start, Vector2 end, Vector2& pos, float t);
+
+void startCutIn();
+
+void DrawCutIn();
+
+void ULTUpdate(char* keys, char* preKeys);
